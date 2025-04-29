a064379d082d1ea02cb058344e3126b44310265b
Geovane551 <Thegeovaneflash@gmail.com>
2025-04-29 18:02:25 -0300

    Aula de comandos de entra e saída

--- 29042025/29402025.py
+++ 29042025/29402025.py
@@ -1,1 +1,11 @@
 print("Calcular área do circuito")
+
+raio = 5.13
+
+area = 3.14 * raio ** 2
+
+print('Área do circuito', area)
+
+print('A área do circuito de raio', raio, 'é', area)
+
+print(f'A área do circuito de raio {raio} é {area:.4f}')
